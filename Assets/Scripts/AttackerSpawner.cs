3db7460518aaa243829f6a89e0dd8d067a09ac2e
malkiev <kevin.attard@gmail.com>
2019-09-22 14:29:09 +0200

    Animations and Sheets

---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    bool spawn = true;
+    [SerializeField] float minSpawnTime = 1;
+    [SerializeField] float maxSpawnTime = 5;
+    [SerializeField] Attacker lizard;
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        while (spawn)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnTime, maxSpawnTime));
+            Spawn();
+        }
+    }
+
+
+    private void Spawn()
+    {
+        Instantiate(lizard, transform.position, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
